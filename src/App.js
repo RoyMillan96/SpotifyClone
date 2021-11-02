2a8b4d795e751668b6db49507810a3abf9491c94
RoyMillan96 <millan.rodrigo201@gmail.com>
2021-11-02 10:07:52 -0600

    create app

---
+++ src/App.js
@@ -0,0 +1,24 @@
+// import logo from './logo.svg';
+import './App.css';
+
+// const NavigationBar = () => {
+
+// }
+
+const App = () => {
+  return (
+    <div className="App">
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <header>
+          <nav>
+            <ul>
+              <li>Home</li>
+              <li>Profile</li>
+            </ul>
+          </nav>
+        </header>
+    </div>
+  );
+}
+
+export default App;
